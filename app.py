59ac829637c0675e7515d49d05a839c7f98d9b73
Shreyash singh <119120856+shreyash81@users.noreply.github.com>
2025-05-03 13:15:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from my first Python Web App on Azure!"
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
